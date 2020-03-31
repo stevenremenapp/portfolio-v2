b3d8c66838b4d511fb3739487901398b39c9c59d
Steven Remenapp <remenapp@gmail.com>
2020-03-31 19:46:26 -0400

    Add gatsby-node.js and blogPostTemplate

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,48 @@
 import React from 'react';
+import { graphql, Link } from "gatsby";
+import styled from 'styled-components';
 import { Layout } from "../components/Layout";
+// import Dump from "../components/Dump";
 
-export default () => {
+const IndexWrapper = styled.main``;
+const PostWrapper = styled.div``;
+
+export default ({ data }) => {
   return (
-    <div>
-      <Layout />
-    </div>
+    <Layout>
+      {/* <Dump data={data} /> */}
+      <IndexWrapper>
+        {data.allMdx.nodes.map(({ id, excerpt, frontmatter, fields }) => (
+          <PostWrapper>
+            <Link to={fields.slug}>
+              <h1>{frontmatter.title}</h1>
+              <p>{frontmatter.date}</p>
+              <p>{excerpt}</p>
+            </Link>
+          </PostWrapper>  
+        ))}
+      </IndexWrapper>
+    </Layout>
   )
 };
+
+export const query = graphql`
+  query SITE_INDEX_QUERY {
+    allMdx(
+      sort: { fields: [frontmatter___date], order: DESC }
+      filter: { frontmatter: { published: { eq: true }}}
+    ) {
+      nodes {
+        id
+        excerpt(pruneLength: 250)
+        frontmatter {
+          title
+          date
+        }
+        fields {
+          slug
+        }
+      }
+    }
+  }
+`;
