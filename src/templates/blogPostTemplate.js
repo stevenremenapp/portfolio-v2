7752c2e4ad38014cf841f67fe3fb54c2230f659e
Steven Remenapp <remenapp@gmail.com>
2020-03-31 23:57:56 -0400

    Add previous and next page navigation to gatsby-node.js and blogPostTemplate

--- src/templates/blogPostTemplate.js
+++ src/templates/blogPostTemplate.js
@@ -1,21 +1,43 @@
 import React from "react";
-import { graphql } from "gatsby";
+import { graphql, Link } from "gatsby";
 import { MDXRenderer } from "gatsby-plugin-mdx";
 import { Layout } from "../components/Layout";
+import Dump from "../components/Dump";
 
-export default ({data}) => {
+export default ({data, pageContext}) => {
   const { frontmatter, body } = data.mdx;
+  const { previous, next } = pageContext;
   return (
     <Layout>
+      <Dump previous={previous} />
+      <Dump next={next} />
       <h1>{frontmatter.title}</h1>
       <p>{frontmatter.date}</p>
       <MDXRenderer>{body}</MDXRenderer>
+      {previous === false ? null : (
+        <div>
+          {previous && (
+            <Link to={previous.fields.slug}>
+              <p>{previous.frontmatter.title}</p>
+            </Link>
+          )}
+        </div>
+      )}
+      {next === false ? null : (
+        <div>
+          {next && (
+            <Link to={next.fields.slug}>
+              <p>{next.frontmatter.title}</p>
+            </Link>
+          )}
+        </div>
+      )}
     </Layout>
   )
 };
 
 export const query = graphql`
-  query PostsBySlug($slug: String!) {
+  query PostBySlug($slug: String!) {
     mdx(fields: { slug: { eq: $slug }}) {
       body
       frontmatter {
